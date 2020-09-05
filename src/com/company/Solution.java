f79c6add6e5cd4fefac7546e91b5a0a3d6ed7741
Igor Koloda <70591141+Igor-Koloda@users.noreply.github.com>
2020-09-05 20:14:06 +0300

    Add files via upload

---
+++ src/com/company/Solution.java
@@ -0,0 +1,13 @@
+package com.company;
+
+
+public class Solution {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+        System.out.println(sb.toString());
+        sb.append(" my").append(" friend");
+        System.out.println(sb.toString());
+
+    }
+}
+
